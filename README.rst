83e7c1deb85104f6fdc41f8a1319260e64f92886
tulip85 <katinka.kromwijk@gmail.com>
2017-09-13 12:58:56 +0200

    first working version

---
+++ README.rst
@@ -0,0 +1,62 @@
+****************************
+Mopidy-chipGPIO
+****************************
+
+.. image:: https://img.shields.io/pypi/v/Mopidy-chipGPIO.svg?style=flat
+    :target: https://pypi.python.org/pypi/Mopidy-chipGPIO/
+    :alt: Latest PyPI version
+
+.. image:: https://img.shields.io/travis/tulip85/mopidy_chipGPIO/master.svg?style=flat
+    :target: https://travis-ci.org/tulip85/mopidy_chipGPIO
+    :alt: Travis CI build status
+
+.. image:: https://img.shields.io/coveralls/tulip85/mopidy_chipGPIO/master.svg?style=flat
+   :target: https://coveralls.io/r/tulip85/mopidy_chipGPIO
+   :alt: Test coverage
+
+Mopidy extension for the NTC CHIP GPIO buttons
+
+
+Installation
+============
+
+Install by running::
+
+    pip install Mopidy-chipGPIO
+
+Or, if available, install the Debian/Ubuntu package from `apt.mopidy.com
+<http://apt.mopidy.com/>`_.
+
+
+Configuration
+=============
+
+Before starting Mopidy, you must add configuration for
+Mopidy-chipGPIO to your Mopidy configuration file::
+
+    [chipGPIO]
+    # TODO: Add example of extension config
+
+
+Project resources
+=================
+
+- `Source code <https://github.com/tulip85/mopidy-chipgpio>`_
+- `Issue tracker <https://github.com/tulip85/mopidy-chipgpio/issues>`_
+
+
+Credits
+=======
+
+- Original author: `tulip <https://github.com/tulip85`__
+- Current maintainer: `tulip <https://github.com/tulip85`__
+- `Contributors <https://github.com/tulip85/mopidy-chipgpio/graphs/contributors>`_
+
+
+Changelog
+=========
+
+v0.1.0 (UNRELEASED)
+----------------------------------------
+
+- Initial release.
---
+++ mopidy_chipGPIO/frontend.py
@@ -0,0 +1,70 @@
+from __future__ import unicode_literals
+import logging
+import traceback
+
+from mopidy import core
+
+import CHIP_IO.GPIO as GPIO
+import time,os
+
+import pykka
+
+
+
+
+logger = logging.getLogger(__name__)
+
+
+class chipGPIO(pykka.ThreadingActor, core.CoreListener):
+
+    def eventDetected(self, channel):
+        if channel == self.config['chipGPIO']['play_pin']:
+            logger.info("pause")
+            if self.core.playback.state.get() == "playing":
+                self.core.playback.pause()
+            elif self.core.playback.state.get() == "stopped":
+                self.core.playback.play()
+            else:
+                self.core.playback.resume()
+            
+        if channel == self.config['chipGPIO']['previous_pin']:
+            logger.info("previous")
+            self.core.playback.previous()
+            
+        if channel == self.config['chipGPIO']['next_pin']:
+            logger.info("next")
+            self.core.playback.next()
+            
+        if channel == self.config['chipGPIO']['stop_pin']:
+            logger.info("stop")
+            self.core.playback.stop()
+
+    def __init__(self, config, core):
+        super(chipGPIO, self).__init__()
+        self.menu = False
+        self.core = core
+        self.config = config
+        
+        logger.info("testing")
+        
+        #register buttons
+        if config['chipGPIO']['play_pin']:
+            GPIO.setup(config['chipGPIO']['play_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(config['chipGPIO']['play_pin'], GPIO.RISING)
+            GPIO.add_event_callback(config['chipGPIO']['play_pin'], self.eventDetected)
+            
+        if config['chipGPIO']['previous_pin']:
+            GPIO.setup(config['chipGPIO']['previous_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(config['chipGPIO']['previous_pin'], GPIO.RISING)
+            GPIO.add_event_callback(config['chipGPIO']['previous_pin'], self.eventDetected)
+            
+        if config['chipGPIO']['next_pin']:
+            GPIO.setup(config['chipGPIO']['next_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(config['chipGPIO']['next_pin'], GPIO.RISING)
+            GPIO.add_event_callback(config['chipGPIO']['next_pin'], self.eventDetected)
+            
+        if config['chipGPIO']['stop_pin']:
+            GPIO.setup(config['chipGPIO']['stop_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(config['chipGPIO']['stop_pin'], GPIO.RISING)
+            GPIO.add_event_callback(config['chipGPIO']['stop_pin'], self.eventDetected)
+        
